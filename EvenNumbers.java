87dce0b797ed0c15c8ec9291a2f5c0f625f99900
Daniel Ostertag <k.d.ostertag@gmail.com>
2017-08-25 17:24:29 -0500

    Create EvenNumbers.java

---
+++ EvenNumbers.java
@@ -0,0 +1,12 @@
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        // Write your code here
+        int counter = 2;
+        while (counter <= 100){
+            System.out.println(counter);
+            counter += 2;
+        }
+    }
+}
