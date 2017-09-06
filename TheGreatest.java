b49a35b3a9684d9c1e14319911ff65d2ac51bb5f
Daniel Ostertag <k.d.ostertag@gmail.com>
2017-09-06 18:32:06 -0500

    Create TheGreatest.java

---
+++ TheGreatest.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class TheGreatest {
+    public static int greatest(ArrayList<Integer> list) {
+        int maximum = -35000;
+        for(int item: list){
+            if(item > maximum){
+                maximum = item;
+            }
+        }
+        // maximum = Collections.max(list);
+        // write code here
+        return maximum;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista.add(3);
+        lista.add(2);
+        lista.add(7);
+        lista.add(2);
+        
+        System.out.println("The greatest number is: " + greatest(lista));
+    }
+}
