c6a54430c74310a6b3dd2e8e60803592f608edcd
Daniel Ostertag <k.d.ostertag@gmail.com>
2017-10-04 20:40:36 -0500

    Update Students.java

--- Students.java
+++ Students.java
@@ -32,3 +32,47 @@ public class Student {
     }
     
 }
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write here the main program
+
+        ArrayList<Student> list = new ArrayList<Student>();
+        Scanner reader = new Scanner(System.in);
+        
+        String userInputName = "";
+        String userInputNumber = "";
+        String userInputSearch = "";
+        
+        while (true) {
+            System.out.print("name: ");
+            userInputName = reader.nextLine();
+            if (userInputName.equals("")) {
+                break;
+            }
+            System.out.print("student number: ");
+            userInputNumber = reader.nextLine();
+            list.add(new Student(userInputName, userInputNumber));
+        }
+        for (Student stu : list) {
+            System.out.println(stu.getName() + " (" + stu.getStudentNumber() + ")");
+        }
+        
+        System.out.println("Give search term:");
+        userInputSearch = reader.nextLine();
+        
+        for (Student stu : list) {
+            if (stu.getName().contains(userInputSearch)) {
+                System.out.println("Result: ");
+                System.out.println(stu.toString());
+            }
+        }
+        
+        
+    }
+}
